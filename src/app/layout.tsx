483f0b37dca788a195f20bacf9a14eccecb91f1b
Gabriel Fonseca <fonsecagabriel9781@gmail.com>
2024-01-15 17:40:48 -0300

    finished responsive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import type { Metadata } from 'next'
 // eslint-disable-next-line camelcase
 import { Nunito_Sans, Roboto } from 'next/font/google'
@@ -34,6 +35,30 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '1087262959085545');
+                    fbq('track', 'PageView');
+                    `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            className="hidden"
+            src="https://www.facebook.com/tr?id=1087262959085545&ev=PageView&noscript=1"
+          />
+        </noscript>
+
         <link
           rel="preload"
           href="https://player-vz-cab8bf78-de6.tv.pandavideo.com.br/embed/css/styles.css"
